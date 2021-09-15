0dae6c2cf3970f6fef14f8d78d77b82f0cdfd535
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-15 20:45:33 -0300

    added: Bottom bar navigation

--- Flutter/use_academy_app/lib/main.dart
+++ Flutter/use_academy_app/lib/main.dart
@@ -14,16 +14,30 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Use Academy"),
+          centerTitle: true,
+        ),
+        backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: const Color.fromRGBO(255, 255, 255, 1),
+          unselectedItemColor: const Color.fromRGBO(50, 50, 50, 1),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: "Perfil",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shop),
+              label: "Sacola",
+            ),
+          ],
+        ),
+      ),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
   }
 }
