0de28dd2ba9d2d06f50f307cf3ef79de5eb09c1d
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-16 19:53:07 -0300

    refactor: move test page to stateless widget and use home page

--- Flutter/use_academy_app/lib/main.dart
+++ Flutter/use_academy_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'ui/pages/pages.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,57 +13,68 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Use Academy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        drawer: const Drawer(),
-        appBar: AppBar(
-          title: const Text("Use Academy"),
-          centerTitle: true,
-          actions: const [
-            Padding(
-              padding: EdgeInsets.only(right: 8.0),
-              child: Icon(Icons.book),
-            ),
-          ],
-          leading: const Icon(Icons.ac_unit),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(15),
-            ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class TestPage extends StatelessWidget {
+  const TestPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const Drawer(),
+      appBar: AppBar(
+        title: const Text("Use Academy"),
+        centerTitle: true,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: Icon(Icons.book),
+          ),
+        ],
+        leading: const Icon(Icons.ac_unit),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(15),
           ),
         ),
-        backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-        body: SafeArea(
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const [
-              MyContainer(),
-              MyContainer(),
-              MyContainer(),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          backgroundColor: Theme.of(context).primaryColor,
-          selectedItemColor: const Color.fromRGBO(255, 255, 255, 1),
-          unselectedItemColor: const Color.fromRGBO(50, 50, 50, 1),
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined),
-              label: "Perfil",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shop),
-              label: "Sacola",
-            ),
+      ),
+      backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+      body: SafeArea(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            MyContainer(),
+            MyContainer(),
+            MyContainer(),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: const Color.fromRGBO(255, 255, 255, 1),
+        unselectedItemColor: const Color.fromRGBO(50, 50, 50, 1),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: "Perfil",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: "Sacola",
+          ),
+        ],
       ),
     );
   }
