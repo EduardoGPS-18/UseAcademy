c6f5e4b98bc2b489097e82b8d6cf6fc7cc8339fc
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-16 21:10:46 -0300

    feat: using GridView

--- Flutter/use_academy_app/lib/ui/pages/home_page.dart
+++ Flutter/use_academy_app/lib/ui/pages/home_page.dart
@@ -11,43 +11,22 @@ class HomePage extends StatelessWidget {
           child: SizedBox(
             width: 64,
             height: 64,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  child: const Text("Elevated"),
-                  onPressed: () => debugPrint("Pressed"),
-                  onLongPress: () => debugPrint("Long press"),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    onPrimary: Colors.green,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 16,
-                      horizontal: 32,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32),
-                    ),
-                  ),
-                ),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: const Text("Outlined"),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text("Text"),
-                ),
-              ],
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+              ),
+              itemCount: 16,
+              physics: BouncingScrollPhysics(),
+              
+              itemBuilder: (ctx, i) => Container(
+                color: Colors.green,
+                child: Center(child: Text("$i")),
+              ),
             ),
           ),
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.red[400],
       ),
     );
   }
